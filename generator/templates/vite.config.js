2f4071048feb283fc31c23eca9731bd89e9edba2
Pandey Pranjali (DES DOS SW SWEX SWF) <Pranjali.Pandey@infineon.com>
2025-01-30 15:30:00 +0530

    changed vite config for web to work

--- generator/templates/vite.config.js
+++ generator/templates/vite.config.js
@@ -1,19 +1,20 @@
-import { fileURLToPath, URL } from "node:url";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url';
+import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
-  root: fileURLToPath(new URL("./src/renderer", import.meta.url)), // Specify the root directory
+  base: './', // Ensure paths are relative
+  root: fileURLToPath(new URL('./src/renderer', import.meta.url)),
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src/renderer/src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src/renderer/src', import.meta.url)),
     },
   },
   css: {
     preprocessorOptions: {
-      scss: { 
-        silenceDeprecations: ["legacy-js-api"],
+      scss: {
+        silenceDeprecations: ['legacy-js-api'],
       },
     },
   },
@@ -21,21 +22,26 @@ export default defineConfig({
     vue({
       template: {
         compilerOptions: {
-          isCustomElement: (tag) => tag.includes("ifx-"),
+          isCustomElement: (tag) => tag.includes('ifx-'),
         },
       },
     }),
     eslintPlugin(),
   ],
   build: {
-    outDir: fileURLToPath(new URL("./dist", import.meta.url)), // Output directory for the web build
+    outDir: fileURLToPath(new URL('./web', import.meta.url)),
     emptyOutDir: true,
     rollupOptions: {
-      input: fileURLToPath(new URL("./src/renderer/index.html", import.meta.url)), // Entry point for the build
+      input: fileURLToPath(new URL('./src/renderer/index.html', import.meta.url)),
       output: {
         chunkFileNames: '[name]-[hash].js',
         entryFileNames: '[name]-[hash].js',
         assetFileNames: '[name]-[hash].[ext]',
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
       },
     },
   }
